9e9868d59be84fe95e2493f3be3e284125f8c368
biggiebroo <sarvar.kulmatov.bigbro@gmail.com>
2023-09-23 17:29:19 +0200

    Changes

--- script.groovy
+++ script.groovy
@@ -7,6 +7,8 @@ def dockerApp() {
   withCredentials([usernamePassword(credentialsId : 'docker', usernameVariable : 'USER', passwordVariable : 'PASS')]) {
 
   sh "docker login -u ${USER} -p ${PASS}"
+  sh "docker build -t biggiebroo/practice:jvm-1.4 ."
+  sh "docker push biggiebroo/practice:1.4"
 
   }
 
